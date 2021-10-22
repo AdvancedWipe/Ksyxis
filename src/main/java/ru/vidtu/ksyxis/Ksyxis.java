7fb0e8d19e459b1fabf6442e89c10446716accd7
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-10-22 18:45:33 +0300

    Release

--- src/main/java/ru/vidtu/ksyxis/Ksyxis.java
+++ src/main/java/ru/vidtu/ksyxis/Ksyxis.java
@@ -3,13 +3,25 @@ package ru.vidtu.ksyxis;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraftforge.fml.common.Mod;
 
-public class Ksyxis implements ModInitializer {
+@Mod("ksyxis")
+public class Ksyxis {
 	public static final Logger LOG = LogManager.getLogger("Ksyxis");
-	@Override
-	public void onInitialize() {
+	public Ksyxis() {
 		LOG.warn("Ksyxis will speedup your world loading. :P");
 		LOG.warn("BUT IT MAY CAUSE ISSUES (INCLUDING COMPAT). DON'T ASK OTHER MODS' AUTHORS FOR COMPAT!");
+		try {
+			Class.forName("org.spongepowered.asm.mixin.Mixins");
+		} catch (Throwable t) {
+			LOG.error("======== Ksyxis ========");
+			LOG.error("MIXIN IS NOT FOUND!");
+			LOG.error("THIS IS NOT A MOD ERROR!");
+			LOG.error("YOU MUST INSTALL ONE OF:");
+			LOG.error("https://github.com/LXGaming/MixinBootstrap/releases");
+			LOG.error("https://github.com/IzzelAliz/MixinLoader/releases");
+			LOG.error("(or another Mixin library)");
+			throw new RuntimeException("Mixin is not loaded. Install MixinBootstrap/MixinLoader.");
+		}
 	}
 }
--- src/main/java/ru/vidtu/ksyxis/mixins/MinecraftServerMixin.java
+++ src/main/java/ru/vidtu/ksyxis/mixins/MinecraftServerMixin.java
@@ -6,13 +6,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.WorldGenerationProgressListener;
+import net.minecraft.world.chunk.listener.IChunkStatusListener;
 import ru.vidtu.ksyxis.Ksyxis;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
-	@Inject(method = "prepareStartRegion", at = @At("HEAD"), cancellable = true)
-	public void onPrepareStartReg(WorldGenerationProgressListener wgpl, CallbackInfo ci) {
+	@Inject(method = "prepareLevels", at = @At("HEAD"), cancellable = true)
+	public void onPrepareLvls(IChunkStatusListener icsl, CallbackInfo ci) {
 		Ksyxis.LOG.info("Not the long loadi-");
 		ci.cancel();
 	}
