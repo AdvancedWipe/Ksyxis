5d16535803b23a2a941eb27e548f36d38b28c4fa
VidTuGit <60929459+VidTuGit@users.noreply.github.com>
2021-10-24 15:16:24 +0300

    Update to 1.1.

--- src/main/java/ru/vidtu/ksyxis/mixins/MinecraftServerMixin.java
+++ src/main/java/ru/vidtu/ksyxis/mixins/MinecraftServerMixin.java
@@ -2,18 +2,17 @@ package ru.vidtu.ksyxis.mixins;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.server.level.ServerChunkCache;
 import ru.vidtu.ksyxis.Ksyxis;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
-	@Inject(method = "prepareLevels", at = @At("HEAD"), cancellable = true)
-	public void onPrepareLvls(ChunkProgressListener cpl, CallbackInfo ci) {
+	@Redirect(method = "prepareLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerChunkCache;getTickingGenerated()I"))
+	public int onPrepareLvls_redirectTickingGenerated(ServerChunkCache scc) {
 		Ksyxis.LOG.info("Not the long loadi-");
-		ci.cancel();
+		return 441;
 	}
 }
